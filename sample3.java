5528d0735ce4abd790b12002ab9e011e1fd7e281
JohnEfrainManalo <mjohnefrain@gmail.com>
2026-09-09 23:38:16 +0800

    Fixed file names and classes name

--- sample3.java
+++ sample3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class sample3{
+class Sample3{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your name: ");
